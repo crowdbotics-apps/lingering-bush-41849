a9058dd0929230a00e0ca4fb375df0c95037c08a
Crowdbotics <team@crowdbotics.com>
2023-07-01 22:09:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lingering-bush-41849",
+  url: "https://lingering-bush-41849.botics.co",
+  api: "https://lingering-bush-41849.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
